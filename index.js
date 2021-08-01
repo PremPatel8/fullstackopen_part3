30e0bfdae10b56d7e183644c6882792b4718a858
Prem Patel <prempatel_8@outlook.com>
2021-08-01 03:41:03 -0500

    3.8*: Phonebook backend step8

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+const { json } = require("body-parser");
 const express = require("express");
 var morgan = require('morgan')
 
 const app = express();
 app.use(express.json());
-app.use(morgan('tiny'))
+morgan.token('data', function (req, res) { return JSON.stringify(req.body) })
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
 let persons = [
   {
