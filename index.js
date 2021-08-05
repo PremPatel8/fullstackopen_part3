39fd1a41a83760143f3f1011494cfe05b0c1fc16
Prem Patel <prempatel_8@outlook.com>
2021-08-04 21:26:17 -0500

    uibuild

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const { json } = require("body-parser");
 const express = require("express");
-var morgan = require('morgan')
+var morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
-morgan.token('data', function (req, res) { return JSON.stringify(req.body) })
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
+app.use(express.static("build"));
+
+morgan.token("data", function (req, res) {
+  return JSON.stringify(req.body);
+});
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :data")
+);
 
 let persons = [
   {
@@ -108,7 +117,7 @@ app.post("/api/persons", (request, response) => {
   response.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
